c4bb69b4762526092dd868b457a49c12ed028be6
JP Rojas Hernandez <juan.rojas@newsela.com>
2022-09-08 18:10:03 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# mvp-design-tokens-lib-ios
+
+[![CI Status](https://img.shields.io/travis/JP Rojas Hernandez/mvp-design-tokens-lib-ios.svg?style=flat)](https://travis-ci.org/JP Rojas Hernandez/mvp-design-tokens-lib-ios)
+[![Version](https://img.shields.io/cocoapods/v/mvp-design-tokens-lib-ios.svg?style=flat)](https://cocoapods.org/pods/mvp-design-tokens-lib-ios)
+[![License](https://img.shields.io/cocoapods/l/mvp-design-tokens-lib-ios.svg?style=flat)](https://cocoapods.org/pods/mvp-design-tokens-lib-ios)
+[![Platform](https://img.shields.io/cocoapods/p/mvp-design-tokens-lib-ios.svg?style=flat)](https://cocoapods.org/pods/mvp-design-tokens-lib-ios)
+
+## Example
+
+To run the example project, clone the repo, and run `pod install` from the Example directory first.
+
+## Requirements
+
+## Installation
+
+mvp-design-tokens-lib-ios is available through [CocoaPods](https://cocoapods.org). To install
+it, simply add the following line to your Podfile:
+
+```ruby
+pod 'mvp-design-tokens-lib-ios'
+```
+
+## Author
+
+JP Rojas Hernandez, juan.rojas@newsela.com
+
+## License
+
+mvp-design-tokens-lib-ios is available under the MIT license. See the LICENSE file for more info.
---
+++ mvp-design-tokens-lib-ios/Classes/StyleDictionary+Struct.swift
@@ -0,0 +1,84 @@
+
+//
+// StyleDictionary+Struct.swift
+//
+
+// Do not edit directly
+// Generated on Thu, 08 Sep 2022 22:44:42 GMT
+
+
+import UIKit
+
+public struct StyleDictionaryStruct {
+    public static let colorBaseGrayDark = UIColor(red: 0.067, green: 0.067, blue: 0.067, alpha: 1)
+    public static let colorBaseGrayLight = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
+    public static let colorBaseGrayMedium = UIColor(red: 0.600, green: 0.600, blue: 0.600, alpha: 1)
+    public static let colorBaseGreen = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
+    public static let colorBaseRed = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let colorFontBase = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let colorFontSecondary = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
+    public static let colorFontTertiary = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
+    public static let colorUiCoreBlue100 = UIColor(red: 0.906, green: 0.945, blue: 1.000, alpha: 1)
+    public static let colorUiCoreBlue300 = UIColor(red: 0.447, green: 0.675, blue: 1.000, alpha: 1)
+    public static let colorUiCoreBlue50 = UIColor(red: 0.906, green: 0.945, blue: 1.000, alpha: 1)
+    public static let colorUiCoreBlue500 = UIColor(red: 0.063, green: 0.435, blue: 0.953, alpha: 1)
+    public static let colorUiCoreBlue700 = UIColor(red: 0.000, green: 0.322, blue: 0.769, alpha: 1)
+    public static let colorUiDarkBlue100 = UIColor(red: 0.016, green: 0.200, blue: 0.478, alpha: 1)
+    public static let colorUiDarkBlue300 = UIColor(red: 0.016, green: 0.200, blue: 0.478, alpha: 1)
+    public static let colorUiDarkBlue50 = UIColor(red: 0.016, green: 0.200, blue: 0.478, alpha: 1)
+    public static let colorUiDarkBlue500 = UIColor(red: 0.016, green: 0.200, blue: 0.478, alpha: 1)
+    public static let colorUiDarkBlue700 = UIColor(red: 0.016, green: 0.200, blue: 0.478, alpha: 1)
+    public static let colorUiGreen100 = UIColor(red: 0.000, green: 1.000, blue: 0.498, alpha: 0.3)
+    public static let colorUiGreen300 = UIColor(red: 0.145, green: 0.753, blue: 0.608, alpha: 1)
+    public static let colorUiGreen50 = UIColor(red: 0.000, green: 1.000, blue: 0.498, alpha: 0.3)
+    public static let colorUiGreen500 = UIColor(red: 0.059, green: 0.525, blue: 0.439, alpha: 1)
+    public static let colorUiGreen700 = UIColor(red: 0.090, green: 0.471, blue: 0.424, alpha: 1)
+    public static let colorUiGrey100 = UIColor(red: 0.463, green: 0.463, blue: 0.463, alpha: 1)
+    public static let colorUiGrey300 = UIColor(red: 0.463, green: 0.463, blue: 0.463, alpha: 1)
+    public static let colorUiGrey50 = UIColor(red: 0.463, green: 0.463, blue: 0.463, alpha: 1)
+    public static let colorUiGrey500 = UIColor(red: 0.463, green: 0.463, blue: 0.463, alpha: 1)
+    public static let colorUiGrey700 = UIColor(red: 0.329, green: 0.329, blue: 0.329, alpha: 1)
+    public static let colorUiGreyDark100 = UIColor(red: 0.200, green: 0.200, blue: 0.200, alpha: 1)
+    public static let colorUiGreyDark300 = UIColor(red: 0.200, green: 0.200, blue: 0.200, alpha: 1)
+    public static let colorUiGreyDark50 = UIColor(red: 0.200, green: 0.200, blue: 0.200, alpha: 1)
+    public static let colorUiGreyDark500 = UIColor(red: 0.200, green: 0.200, blue: 0.200, alpha: 1)
+    public static let colorUiGreyDark700 = UIColor(red: 0.114, green: 0.114, blue: 0.114, alpha: 1)
+    public static let colorUiGreyExtraLight100 = UIColor(red: 0.973, green: 0.973, blue: 0.973, alpha: 1)
+    public static let colorUiGreyExtraLight300 = UIColor(red: 0.973, green: 0.973, blue: 0.973, alpha: 1)
+    public static let colorUiGreyExtraLight50 = UIColor(red: 0.973, green: 0.973, blue: 0.973, alpha: 1)
+    public static let colorUiGreyExtraLight500 = UIColor(red: 0.973, green: 0.973, blue: 0.973, alpha: 1)
+    public static let colorUiGreyExtraLight700 = UIColor(red: 0.973, green: 0.973, blue: 0.973, alpha: 1)
+    public static let colorUiGreyLight100 = UIColor(red: 0.867, green: 0.867, blue: 0.867, alpha: 1)
+    public static let colorUiGreyLight300 = UIColor(red: 0.737, green: 0.737, blue: 0.745, alpha: 1)
+    public static let colorUiGreyLight50 = UIColor(red: 0.918, green: 0.922, blue: 0.925, alpha: 1)
+    public static let colorUiGreyLight500 = UIColor(red: 0.584, green: 0.584, blue: 0.584, alpha: 1)
+    public static let colorUiGreyLight700 = UIColor(red: 0.584, green: 0.584, blue: 0.584, alpha: 1)
+    public static let colorUiRed100 = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 0.3)
+    public static let colorUiRed300 = UIColor(red: 1.000, green: 0.420, blue: 0.420, alpha: 1)
+    public static let colorUiRed50 = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 0.3)
+    public static let colorUiRed500 = UIColor(red: 0.867, green: 0.212, blue: 0.212, alpha: 1)
+    public static let colorUiRed700 = UIColor(red: 0.765, green: 0.129, blue: 0.075, alpha: 1)
+    public static let colorUiWhite100 = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let colorUiWhite300 = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let colorUiWhite50 = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let colorUiWhite500 = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let colorUiWhite700 = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let colorUiYellow100 = UIColor(red: 1.000, green: 0.788, blue: 0.455, alpha: 1)
+    public static let colorUiYellow300 = UIColor(red: 1.000, green: 0.788, blue: 0.455, alpha: 1)
+    public static let colorUiYellow50 = UIColor(red: 1.000, green: 0.788, blue: 0.455, alpha: 1)
+    public static let colorUiYellow500 = UIColor(red: 1.000, green: 0.788, blue: 0.455, alpha: 1)
+    public static let colorUiYellow700 = UIColor(red: 1.000, green: 0.788, blue: 0.455, alpha: 1)
+    public static let componentsBadgeUiColorCoreBlueColor = UIColor(red: 0.000, green: 0.322, blue: 0.769, alpha: 1)
+    public static let componentsBadgeUiColorGreyColor = UIColor(red: 0.463, green: 0.463, blue: 0.463, alpha: 1)
+    public static let componentsBadgeUiColorGreyDarkColor = UIColor(red: 0.200, green: 0.200, blue: 0.200, alpha: 1)
+    public static let componentsBadgeUiColorRedColor = UIColor(red: 0.765, green: 0.129, blue: 0.075, alpha: 1)
+    public static let componentsButtonStatusColorDangerColor = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let paletteActionsDanger = UIColor(red: 0.765, green: 0.129, blue: 0.075, alpha: 1)
+    public static let paletteActionsPrimary = UIColor(red: 0.000, green: 0.322, blue: 0.769, alpha: 1)
+    public static let paletteActionsSecondary = UIColor(red: 0.200, green: 0.200, blue: 0.200, alpha: 1)
+    public static let paletteActionsTertiary = UIColor(red: 0.463, green: 0.463, blue: 0.463, alpha: 1)
+    public static let sizeFontBase = CGFloat(16.00) /* the base size of the font */
+    public static let sizeFontLarge = CGFloat(32.00) /* the large size of the font */
+    public static let sizeFontMedium = CGFloat(16.00) /* the medium size of the font */
+    public static let sizeFontSmall = CGFloat(12.00) /* the small size of the font */
+}
